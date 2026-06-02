987803bc035a5565d0abaa624060f19b8cd630cb
Bruno Mocellin <bruno.mocellin@testingcompany.com.br>
2026-06-02 09:18:36 -0300

    fix/ spa route

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: process.env.VITE_BASENAME || "/",
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": "http://localhost:3000"
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: env.VITE_BASENAME || "/",
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": "http://localhost:3000"
+      }
+    },
+    build: {
+      outDir: "dist"
     }
-  },
-  build: {
-    outDir: "dist"
-  }
+  };
 });
